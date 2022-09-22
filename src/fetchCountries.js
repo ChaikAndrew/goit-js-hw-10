9c78103f213ec538f164376b4b365f82d204be69
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-09-22 04:09:38 +0200

    fetchCountries / imports

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const fieldsName = '?fields=name,capital,currencies,flags,languages'
+
+function fetchCountries(name) {
+ return fetch(`${BASE_URL}${name}${fieldsName}`)
+ .then (res => {
+    if (!res.ok) {
+        throw new Error(res.status);
+    }
+    return res.json();
+ });
+}
+
+export { fetchCountries };
