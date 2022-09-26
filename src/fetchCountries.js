b650b0b19267b31dcd9b7534f33ba8e1e02e989e
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-09-26 16:28:32 +0200

    add index.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
-const fieldsName = '?fields=name,capital,currencies,flags,languages'
+const fieldsName = '?fields=name,capital,population,flags,languages'
 
 function fetchCountries(name) {
  return fetch(`${BASE_URL}${name}${fieldsName}`)
